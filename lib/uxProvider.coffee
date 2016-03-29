fuse-open/atom-plugin

commit 0da256f982a8e397b47239010a9f09ad01c5254f

Fix code completion bug
Suggestions didn't work near '/>'. However side effect of this commit is that suggestions will now also be shown between '/' and  '>', which isn't expected.

lib/uxProvider.coffee
@@ -15,8 +15,7 @@ class UXProvider
   getSuggestions: ({editor, bufferPosition, scopeDescriptor, prefix, activatedManually}) ->
     # Check if we are around a '/>'
     scopes = scopeDescriptor.scopes
-    if scopes.indexOf('punctuation.definition.tag.xml') isnt -1 or
-        scopes[scopes.length - 1] == 'text.ux'
+    if scopes[scopes.length - 1] == 'text.ux'
       return []
 
     new Promise (resolve) =>

commit d3e822cf63a4b4351cacbcf0b2d3ac7b72bd8e1b

Fix suggestion regression from 0da256f982a8e397b47239010a9f09ad01c5254f

lib/uxProvider.coffee
@@ -1,4 +1,5 @@
 {GetCodeSuggestionsRequest} = require './messages'
+{Range} = require 'atom'
 
 module.exports =
 class UXProvider
@@ -13,9 +14,15 @@ class UXProvider
   constructor: (@daemon) ->
 
   getSuggestions: ({editor, bufferPosition, scopeDescriptor, prefix, activatedManually}) ->
-    # Check if we are around a '/>'
     scopes = scopeDescriptor.scopes
-    if scopes[scopes.length - 1] == 'text.ux'
+
+    # Check if we are around a '/>'
+    # Or in a inner element area
+    nextTwoChars = editor.getTextInBufferRange(
+      new Range(bufferPosition, bufferPosition.translate([0,2])))
+    isEndOfScope = scopes.indexOf('punctuation.definition.tag.xml') isnt -1 and nextTwoChars != "/>"
+
+    if isEndOfScope or scopes[scopes.length - 1] == 'text.ux'
       return []
 
     new Promise (resolve) =>